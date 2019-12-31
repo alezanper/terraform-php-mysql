a5b52c4eb4a456c07aae26ed827dda77c7e72018
Alexander Benavides <fabenavideso@gmail.com>
2019-12-30 21:21:40 -0500

    updating terraform

--- Terraform-php-mysql/Terraform/main.tf
+++ Terraform-php-mysql/Terraform/main.tf
@@ -13,9 +13,10 @@ resource "azurerm_resource_group" "test" {
 }
 
 resource "azurerm_app_service_plan" "test" {
-  name                = "example-appserviceplan"
+  name                = "terraform-appplan"
   location            = "${azurerm_resource_group.test.location}"
   resource_group_name = "${azurerm_resource_group.test.name}"
+  kind                = "Linux"
 
   sku {
     tier = "Standard"
@@ -30,8 +31,8 @@ resource "azurerm_app_service" "test" {
   app_service_plan_id = "${azurerm_app_service_plan.test.id}"
 
   site_config {
-    dotnet_framework_version = "v4.0"
-    scm_type                 = "LocalGit"
+    linux_fx_version = "PHP|7.3"
+    scm_type         = "LocalGit"
   }
 
   app_settings = {
@@ -50,8 +51,8 @@ resource "azurerm_sql_server" "test" {
   resource_group_name          = "${azurerm_resource_group.test.name}"
   location                     = "${azurerm_resource_group.test.location}"
   version                      = "12.0"
-  administrator_login          = "houssem"
-  administrator_login_password = "4-v3ry-53cr37-p455w0rd"
+  administrator_login          = "newuser"
+  administrator_login_password = "password"
 }
 
 resource "azurerm_sql_database" "test" {
